c6a4682294f8cfee1bbe28be688b787a9eb8e799
vetst <vetstv@gmail.com>
2020-12-01 20:59:37 +0300

    Third commit

--- spring-boot/src/main/java/web/config/security/SecurityConfig.java
+++ spring-boot/src/main/java/web/config/security/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import web.config.handler.LoginSuccessHandler;
 import web.service.UserServiceImpl;
